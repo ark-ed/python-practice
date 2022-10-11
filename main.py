354528e0440698aac53f104a7f68e11fe3a6daa8
arkved <115508147+ark-ed@users.noreply.github.com>
2022-10-11 00:50:22 +0000

    init

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-#git commit -a
+#init
 
 import sqlite3
 
 con = sqlite3.connect('database.db')
 cur = con.cursor()
 
-cur.execute("CREATE TABLE worker(name,hours_worked,salary")
+#cur.execute("CREATE TABLE worker(name,hours_worked,salary)")
+
+cur.execute("""
+    INSERT INTO worker VALUES
+        ('Michael', 8, 12),
+        ('Jake', 9, 14),
+        ('Tod', 7, 12)
+""")
+
+con.commit()

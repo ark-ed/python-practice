fe42ca091303695cf26dd90adf9aa158ff7f4434
ark-ed <115508147+ark-ed@users.noreply.github.com>
2022-10-11 01:16:53 +0000

    init

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-#init
+# Practice on sqlite3
+# Documentation: https://docs.python.org/3/library/sqlite3.html
 
 import sqlite3
 
-con = sqlite3.connect('database.db')
+con = sqlite3.connect('employees.db')
 cur = con.cursor()
 
 #cur.execute("CREATE TABLE worker(name,hours_worked,salary)")
 
-cur.execute("""
-    INSERT INTO worker VALUES
-        ('Michael', 8, 12),
-        ('Jake', 9, 14),
-        ('Tod', 7, 12)
-""")
+#cur.execute("""
+#    INSERT INTO worker VALUES
+#        ('Michael', 8, 12),
+#        ('Jake', 9, 14),
+#        ('Tod', 7, 12)
+#""")
+
+#data = [
+#    ('Greg', 12, 13),
+#    ('Addison', 8, 12),
+#    ('Jessica', 9, 13),
+#]
+
+#cur.executemany("INSERT INTO worker VALUES (?,?,?)",data)
 
 con.commit()

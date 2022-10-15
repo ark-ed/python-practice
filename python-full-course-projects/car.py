42d21168f574278d8e5976c036ede48773a164a7
ark-ed <115508147+ark-ed@users.noreply.github.com>
2022-10-15 21:09:00 +0000

    init

--- python-full-course-projects/car.py
+++ python-full-course-projects/car.py
@@ -1,8 +1,8 @@
 # Car Game
 # Python Tutorial - Python Full Course for Beginners by Programming with Mosh
-# Attempt 1
 
 print("Type 'help' to view all commands")
+cu_i = "stop"
 
 while True:
     u_i = input(">")
@@ -14,11 +14,23 @@ while True:
     > quit: exit the program    
     """)
 
-    elif u_i.lower() == "start":
-        print("Started the vehicle")
-    
-    elif u_i.lower() == "stop":
-        print("Stopped the vehicle")
+    #---------------------------------------------
+    # Start
+    elif u_i == "start" and cu_i == "stop":
+      print("Started the vehicle")
+      cu_i = "start"
+    elif u_i == "start" and cu_i == "start":
+      print("Vehicle is already on")
+  
+    # Stop
+    elif u_i.lower() == "stop" and cu_i.lower() == "start":
+      print("Stopped the vehicle")
+      cu_i = "stop"
+    elif u_i.lower() == "stop" and cu_i == "stop":
+      print("Vehicle is already off")
 
     elif u_i.lower() == "quit":
         exit()
+
+    else:
+      print("Sorry, I don't understand")
